01f9fd0e32fc93f1815790fca60ab46b910f9b75
aspehkonen <aspehkonen@gmail.com>
2017-06-09 10:21:38 +0200

    First Theme

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div> <!-- site content -->
+
+<?php get_sidebar(); ?>
+
+</div> <!-- site container -->
+
+<div id="site-footer">
+	<p>&copy; My First Theme</p>	
+</div>
+
+<?php wp_footer() ?>
+</body>
+</html>
